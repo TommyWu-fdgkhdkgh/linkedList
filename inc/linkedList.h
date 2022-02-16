ed019853162a030771f6858e9372cbba1b811ed6
chien-hsing.wu <fdgkhdkgh@gmail.com>
2022-02-17 02:29:57 +0800

    Update interface

--- inc/linkedList.h
+++ inc/linkedList.h
@@ -6,25 +6,26 @@ struct dlNode_s {
   dlNode_t *prev, *next; 
 };
 
-void DlPrePend();
-void DlAppend();
+void DlPrePend(dlNode_t **p2head, dlNode_t *add);
+void DlAppend(dlNode_t **p2head, dlNode_t *add);
 
-void DlPrePendElem();
-void DlAppenElem();
-void DlReplaceElem();
+void DlPrePendElem(dlNode_t **p2head, dlNode_t *add, dlNode_t *elem);
+void DlAppenElem(dlNode_t **p2head, dlNode_t *add, dlNode_t *elem);
+void DlReplaceElem(dlNode_t **p2head, dlNode_t *rep, dlNode_t *elem);
 
 void DlInsertInorder(dlNode_t **p2head,
                      dlNode_t *add,
                      int (*cmp)(dlNode_t *a, dlNode_t *b));
-void DlConcat();
+void DlConcat(dlNode_t **p2head1, dlNode_t **p2head2);
 void DlUnLink(dlNode_t **p2head, dlNode_t *del);
-void DlSort();
+void DlSort(dlNode_t **p2head,
+            int (*cmp)(dlNode_t *a, dlNode_t *b));
 
-void DlSearch();
+void DlSearch(dlNode_t *head);
 void DlSearchScalar();
 
 void DlLowerBound();
-void DlCount();
+unsigned long long DlCount(dlNode_t *head);
 
 #define DL_FOREACH(head, elt) \
 	for((elt) = (head);(elt) != NULL;(elt) = (elt)->next) 
